29e0195e633c28d1a546d08d439857582c38bdb1
czl <295183917@qq.com>
2025-03-02 19:11:30 +0800

    完善项目修改bug

--- 快手极速版365天签到.py
+++ 快手极速版365天签到.py
@@ -23,15 +23,14 @@ def check_popup():
 def task_loop():
     last_time = None
     while True:
-        finish_view = d(resourceId="com.kuaishou.nebula:id/kem_activity_task_pendant", className="android.widget.FrameLayout")
-        if finish_view.exists:
-            print("当前已完成任务。")
-            d.press("back")
-            break
         countdown_view = d(resourceId="com.kuaishou.nebula:id/pendant_task_status", className="android.widget.TextView")
         if countdown_view.exists:
             countdown_text = countdown_view.get_text()
             print(countdown_text)
+            if countdown_text == "领取奖励":
+                print("当前已完成任务。")
+                d.press("back")
+                break
             countdown_time = countdown_text.replace("倒计时 ", "")
             if last_time:
                 seconds1 = countdown_to_seconds(last_time)
@@ -51,14 +50,14 @@ earn_btn.click()
 print("进入去赚钱板块")
 time.sleep(8)
 clock_in_view = d(className="android.view.View", textContains="拿好礼")
-if clock_in_view.exists:
+if clock_in_view.exists(timeout=10):
     print("点击拿好礼")
     d.click(clock_in_view.center()[0], clock_in_view.center()[1])
     time.sleep(5)
 else:
     raise Exception("找不到拿好礼按钮，请重启程序")
 print("当前位于打开页面")
-sign_btn = d(className="android.view.View", text="去签到")
+sign_btn = d(className="android.widget.Button", text="去签到")
 if sign_btn.exists:
     print("点击去签到")
     d.click(sign_btn.center()[0], sign_btn.center()[1])
